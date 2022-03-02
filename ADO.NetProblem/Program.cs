5e1aa05b8a40617546833f0fd2886c8b98850e22
Aftab-1998 <alam41976@gmail.com>
2022-03-02 23:13:30 +0530

    [AFTAB1] Add; Added The method to create Payroll Service Database

---
+++ ADO.NetProblem/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO.NetProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+           EmployeeRepository employeeRepository = new EmployeeRepository();
+            employeeRepository.GetAllEmployee();
+            Console.ReadLine();
+        }
+    }
+}
